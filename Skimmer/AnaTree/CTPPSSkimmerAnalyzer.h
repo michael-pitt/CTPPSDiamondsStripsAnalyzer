e1d62afc2861163260657ba0d48cf442daf8fda7
Diego Figueiredo <dmf@cern.ch>
2017-06-19 15:08:28 +0200

    New files.

--- Skimmer/AnaTree/CTPPSSkimmerAnalyzer.h
+++ Skimmer/AnaTree/CTPPSSkimmerAnalyzer.h
@@ -103,11 +103,11 @@ CTPPSSkimmerAnalyzer::CTPPSSkimmerAnalyzer(TTree *tree) : fChain(0)
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("run_295977_no_trigger.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("run_295977_expressphysics.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("run_295977_no_trigger.root");
+         f = new TFile("run_295977_expressphysics.root");
       }
-      TDirectory * dir = (TDirectory*)f->Get("run_295977_no_trigger.root:/Skimmer");
+      TDirectory * dir = (TDirectory*)f->Get("run_295977_expressphysics.root:/Skimmer");
       dir->GetObject("CTPPSSkimmerDiamond",tree);
 
    }
--- Skimmer/test/RunSkimmer.py
+++ Skimmer/test/RunSkimmer.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
 import copy
 
 process = cms.Process("SkimmerCTPPS")
-useAOD = True
 
 #########################
 #    General options    #
@@ -11,19 +11,13 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.options   = cms.untracked.PSet(
 )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #########################
 #      Input files      #
 #########################
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        # Run 295 977
-        '/store/data/Run2017A/ZeroBias/AOD/PromptReco-v1/000/295/977/00000/1476F58C-F249-E711-B12B-02163E01473C.root',
-        '/store/data/Run2017A/ZeroBias/AOD/PromptReco-v1/000/295/977/00000/921E437D-FC49-E711-9CB5-02163E01A549.root',
-        '/store/data/Run2017A/ZeroBias/AOD/PromptReco-v1/000/295/977/00000/FC1DC0D5-EF49-E711-8CFB-02163E01A2E7.root'
-    ),
-)
+from CTPPSDiamondAnalyzer.Skimmer.RunsLowPU_cff import fileNames
+process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(fileNames))
 
 #########################
 #        Triggers       #
@@ -31,7 +25,7 @@ process.source = cms.Source("PoolSource",
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 process.hltFilter = copy.deepcopy(hltHighLevel)
 process.hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-process.hltFilter.HLTPaths = ['HLT_ZeroBias_IsolatedBunches_v*','HLT_ZeroBias_part0_v*','HLT_ZeroBias_part1_v*','HLT_ZeroBias_part2_v*','HLT_ZeroBias_part3_v*','HLT_ZeroBias_part4_v*','HLT_ZeroBias_part5_v*','HLT_ZeroBias_part6_v*','HLT_ZeroBias_part7_v*','HLT_ZeroBias_part8_v*','HLT_ZeroBias_part9_v*']
+process.hltFilter.HLTPaths = ['HLT_ZeroBias_IsolatedBunches_v*','HLT_ZeroBias_v*','HLT_ZeroBias_part0_v*','HLT_ZeroBias_part1_v*','HLT_ZeroBias_part2_v*','HLT_ZeroBias_part3_v*','HLT_ZeroBias_part4_v*','HLT_ZeroBias_part5_v*','HLT_ZeroBias_part6_v*','HLT_ZeroBias_part7_v*','HLT_ZeroBias_part8_v*','HLT_ZeroBias_part9_v*']
 process.hltFilter.throw = cms.bool(False)
 
 #########################
