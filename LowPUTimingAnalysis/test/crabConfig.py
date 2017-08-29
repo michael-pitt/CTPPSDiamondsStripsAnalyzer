f7e76827d6b41df7c5e00aefbc13df5e805fc6e9
Jonathan <jjhollar@cern.ch>
2017-08-29 10:17:22 +0200

    committing low-pu analysis ntuple maker

---
+++ LowPUTimingAnalysis/test/crabConfig.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'ctpps_lowpileup300088_zerobias1'
+config.General.workArea = '/tmp/jjhollar/ctpps_lowpileup300088_zerobias1'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'zb10_ntuple_diamond_rereco_test_cfg.py'
+config.JobType.outputFiles = ['output_ZeroBias1.root']
+
+config.Data.inputDataset = '/ZeroBias1/Run2017C-PromptReco-v2/AOD'
+config.Data.useParent = True
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 25
+config.Data.runRange = '300088'
+
+config.Data.outLFNDirBase = '/store/user/jjhollar/CTPPS_LowPileup300088ZB1/'
+config.Data.publication = False
+config.Data.outputDatasetTag = 'CTPPS_LowPileup300088ZB1'
+config.Site.storageSite = 'T2_CH_CERN'
---
+++ LowPUTimingAnalysis/test/zb10_ntuple_diamond_rereco_test_cfg.py
@@ -0,0 +1,105 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('CTPPS2',eras.Run2_25ns)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.verbosity = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+#    statistics = cms.untracked.vstring(),
+#    destinations = cms.untracked.vstring('cerr'),
+#    cerr = cms.untracked.PSet(
+#        threshold = cms.untracked.string('WARNING')
+#    )
+)
+
+
+process.source = cms.Source("EmptyIOVSource",
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    lastValue = cms.uint64(1),
+    interval = cms.uint64(1)
+)
+
+# Track memory leaks
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",ignoreTotal = cms.untracked.int32(1) )
+
+
+from CTPPSDiamondAnalyzer.LowPUTimingAnalysis.ZeroBias10_300088_cff import readFiles
+from CTPPSDiamondAnalyzer.LowPUTimingAnalysis.ZeroBias10_300088_RAW_cff import secReadFiles
+
+process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(readFiles))
+process.source.secondaryFileNames = cms.untracked.vstring(secReadFiles)
+
+process.mydiamonds = cms.EDAnalyzer(
+    'Diamonds',
+    tagStatus = cms.InputTag("ctppsDiamondRawToDigi", "TimingDiamond", "CTPPS2"),
+    tagDigi = cms.InputTag("ctppsDiamondRawToDigi", "TimingDiamond", "CTPPS2"),
+    tagFEDInfo = cms.InputTag("ctppsDiamondRawToDigi", "TimingDiamond", "CTPPS2"),
+    tagDiamondRecHits = cms.InputTag("ctppsDiamondRecHits"),
+    tagDiamondLocalTracks = cms.InputTag("ctppsDiamondLocalTracks"),
+    tagLocalTrack = cms.InputTag("totemRPLocalTrackFitter"),
+    verticesTag = cms.InputTag('offlinePrimaryVertices'),
+    jetsTag = cms.InputTag('ak4PFJets'),
+    tagParticleFlow = cms.InputTag('particleFlow'),
+    tagRPixDigi = cms.InputTag( "ctppsPixelDigis" ),
+    tagRPixCluster = cms.InputTag( "ctppsPixelClusters" ),
+    tagRPixRecHit = cms.InputTag( "ctppsPixelRecHits" ),
+    outfilename = cms.untracked.string( "output_ZeroBias1.root" )
+)
+
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_hlt_relval', '')
+
+
+# raw-to-digi conversion
+process.load("EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff")
+
+############
+#process.o1 = cms.OutputModule("PoolOutputModule",
+##        outputCommands = cms.untracked.vstring('drop *',
+##                                               'keep *_totemRP*_*_*',
+##                                               'keep *_ctpps*_*_*'
+##    
+#                              outputCommands = cms.untracked.vstring('keep *_*_*_*'
+#),
+#        fileName = cms.untracked.string('/tmp/jjhollar/CTPPS_redigi_rereco_300088_HINCaloJets.root')
+#        )
+
+process.load("RecoCTPPS.Configuration.recoCTPPS_cff")
+
+
+process.ALL = cms.Path(
+    process.ctppsDiamondRawToDigi * 
+    process.recoCTPPS * 
+    process.mydiamonds
+                       )
+
+process.schedule = cms.Schedule(process.ALL)
+
+#process.outpath = cms.EndPath(process.o1)
+
+#process.schedule = cms.Schedule(process.ALL,process.outpath)
+
+# filter all path with the production filter sequence
+#for path in process.paths:
+#    getattr(process,path)._seq =  getattr(process,path)._seq
+
+
+print process.dumpPython()
+
