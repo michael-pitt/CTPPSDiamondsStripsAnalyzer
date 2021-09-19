f2b02fe969eac1abc129afa13eb49ed565bab6ff
michael-pitt <m_u_pitt@yahoo.com>
2021-09-19 03:07:08 +0200

    update crab config

--- LowPUTimingAnalysis/test/crabConfig.py
+++ LowPUTimingAnalysis/test/crabConfig.py
@@ -1,23 +1,26 @@
-from CRABClient.UserUtilities import config, getUsernameFromSiteDB
-config = config()
+from WMCore.Configuration import Configuration
+config = Configuration()
 
-config.General.requestName = 'ctpps_lowpileup300088_zerobias1'
-config.General.workArea = '/tmp/jjhollar/ctpps_lowpileup300088_zerobias1'
-config.General.transferOutputs = True
+config.section_("General")
+config.General.requestName = "testTiming_v1"
+config.General.workArea = "grid"
+config.General.transferOutputs=True
 config.General.transferLogs = True
 
-config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'zb10_ntuple_diamond_rereco_test_cfg.py'
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "RunTimingAnalysisMINIAOD_cfg.py"
 config.JobType.outputFiles = ['output_ZeroBias.root']
 
-config.Data.inputDataset = '/ZeroBias1/Run2017C-PromptReco-v2/AOD'
-config.Data.useParent = True
-config.Data.inputDBS = 'global'
-config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 25
-config.Data.runRange = '300088'
-
-config.Data.outLFNDirBase = '/store/user/jjhollar/CTPPS_LowPileup300088ZB1/'
+config.section_("Data")
+config.Data.inputDataset = "/ZeroBias/Run2017H-09Aug2019_UL2017_LowPU-v1/MINIAOD"
+config.Data.ignoreLocality = False
+config.Data.inputDBS = "global"
+config.Data.lumiMask = '/eos/project/c/ctpps/Operations/DataExternalConditions/2017/combined_RPIN_CMS_2017H.json'
+config.Data.splitting = "LumiBased"
+config.Data.unitsPerJob = 30
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CTPPS_LowPileup300088ZB1'
-config.Site.storageSite = 'T2_CH_CERN'
+config.Data.outLFNDirBase = "/store/group/cmst3/user/mpitt/test/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_CH_CERN"
